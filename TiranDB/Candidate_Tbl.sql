c2b89275f63d6ec2201bc575cc20eab4e0c70343
venkatdev22 <venkat4fz@gmail.com>
2015-02-26 18:57:45 +0530

    candidate tbl updated

--- TiranDB/Candidate_Tbl.sql
+++ TiranDB/Candidate_Tbl.sql
@@ -2,6 +2,9 @@
 (
 CandidateID int NOT NULL IDENTITY(1,1) PRIMARY KEY,
 FirstName varchar(255),
-LastName varchar(255) NULL,
-EmployeeID int NOT NULL
+LastName varchar(255),
+EmployeeID int NOT NULL,
+EmailID varchar(255) NOT NULL,
+Psword varchar(255) NOT NULL,
+ConfirmPsword varchar(255) NOT NULL
 )
