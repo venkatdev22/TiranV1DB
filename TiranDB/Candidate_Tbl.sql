0c02af3b00da80da9c2f27b5d0b20e2c929ebea3
venkatdev22 <venkat4fz@gmail.com>
2015-02-26 19:07:05 +0530

    updated candidate tbl

--- TiranDB/Candidate_Tbl.sql
+++ TiranDB/Candidate_Tbl.sql
@@ -6,5 +6,6 @@ LastName varchar(255),
 EmployeeID int NOT NULL,
 EmailID varchar(255) NOT NULL,
 Psword varchar(255) NOT NULL,
-ConfirmPsword varchar(255) NOT NULL
+ConfirmPsword varchar(255) NOT NULL,
+AvatarUrl varchar(255)
 )
